818177e04cf85e1a7f57590cabfd8ce3d38de98c
staticmario <terrygr99@gmail.com>
2023-03-02 17:17:56 -0500

    ec2 count chnaged from 3 to 5

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+# Create EC2
+resource "aws_instance" "tf_demo" {
+  count                  = 5
+  ami                    = "ami-0b5eea76982371e91"
+  key_name               = "demo-keypair"
+  subnet_id              = aws_subnet.pub_subnet.id
+  vpc_security_group_ids = [aws_security_group.ssh_https_sg.id]
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "tf-instance"
+  }
+}
